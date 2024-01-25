2f6cb5ad2eca624a9efd9813a12b0df85627b0c0
Viraj Patil <virajspatil1998@gmail.com>
2024-01-26 00:13:15 +0530

    added hello.java

---
+++ html_basics/hello.java
@@ -0,0 +1,12 @@
+package html_basics;
+
+public class hello {
+
+    int index;
+    int nextStateValue;
+
+    hello(int index, int nextStateValue) {
+        this.index = index;
+        this.nextStateValue = nextStateValue;
+    }
+}
